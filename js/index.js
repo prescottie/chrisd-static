bf002faf329ab2c04e8f67f6fa11bf577d21e89d
prescottie <scott.hagan@aol.com>
2018-03-14 18:34:00 -0700

    version 1 of chris's static realty website

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+    $('.dropdown-button').dropdown({
+      inDuration: 500,
+      outDuration: 225,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      hover: false, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: true // Stops event propagation
+      }
+    );
+
+});

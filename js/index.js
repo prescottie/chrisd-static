63b8902021372eb16b48d43c01790b1e257ab748
prescottie <scott.hagan@aol.com>
2018-04-28 16:22:51 -0700

    added city listing pages

--- js/index.js
+++ js/index.js
@@ -4,10 +4,17 @@ $( document ).ready(function() {
       outDuration: 225,
       constrainWidth: false, // Does not change width of dropdown to that of the activator
       hover: false, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
+      coverTrigger: false, // Displays dropdown below the button
       alignment: 'left', // Displays dropdown with edge aligned to the left of button
-      stopPropagation: true // Stops event propagation
+      closeOnClick: false,
+      }
+    );
+
+    $('.nested-dropdown-button').dropdown({
+      inDuration: 500,
+      outDuration: 225,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      coverTrigger: false, // Displays dropdown below the button
       }
     );
 
